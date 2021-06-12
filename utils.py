a37fd00026514652158673d71d89dfb880a77535
Giacomo <giamel95@outlook.it>
2021-06-12 17:22:03 +0200

    Implemented stratified sampling

--- utils.py
+++ utils.py
@@ -1,85 +1,62 @@
 # Imports
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-import tensorflow_datasets as tfds
-import sklearn as sk
-from sklearn.model_selection import train_test_split
-
-import tensorflow.keras
-from keras.preprocessing.text import Tokenizer
-from keras.preprocessing.sequence import pad_sequences
-from spark_tensorflow_distributor import MirroredStrategyRunner
-from keras import backend as K
+#import numpy as np
+#import pandas as pd
+#import tensorflow_datasets as tfds
 
 
-# It computes Precision
-def precision(y_true, y_pred):
-    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
-    predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
-    precision = true_positives / (predicted_positives + K.epsilon())
-    return precision
+def load_dataset(spark, file_name="dataset.csv"):
+  """
+  Reads the SMS spam dataset and splits it into training set (0.8) and test set (0.2) by a stratified sampling procedure.
+  It returns both training set and test set as Spark Dataframes.
+  """
+
+  # Load CSV
+  #dataset = spark.read.format("csv").option("header", True).option("multiLine", True).option("escape","\"").load("dataset.csv")
+  dataset = spark.read.format("csv").option("header", True).option("multiLine", True).option("escape","\"").load("hdfs://s01:9000/dataset.csv")
+  dataset = dataset.withColumnRenamed('x', 'text')
+  dataset = dataset.withColumnRenamed('y', 'label')
+
+  dataset = dataset.withColumn('label', dataset['label'].cast('int'))
+
+  # Stratified Sampling: .8 training set / .2 test set
+  # Split dataframes between legitimates and spams
+  legitimates = dataset.filter(dataset['label'] == 0)
+  spams = dataset.filter(dataset['label'] == 1)
+
+  # Split datasets into training set and test set
+  train_legit, test_legit = legitimates.randomSplit([0.8,0.2], seed=0)
+  train_spam, test_spam = spams.randomSplit([0.8,0.2], seed=0)
+
+  # Merge datasets
+  training_set = train_legit.union(train_spam)
+  test_set = test_legit.union(test_spam)
+
+  return training_set, test_set
 
 
-# It computes Recall
-def recall(y_true, y_pred):
-    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
-    possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
-    recall = true_positives / (possible_positives + K.epsilon())
-    return recall
+'''
+def load_sms_spam_data():
+    #tfds.list_builders()
 
+    # Loading data
+    ds = tfds.load('huggingface:sms_spam', split='train', shuffle_files=True)
+    assert isinstance(ds, tf.data.Dataset)
 
-# It computes F-Measure
-def f1(y_true, y_pred):
-    p = precision(y_true, y_pred)
-    r = recall(y_true, y_pred)
-    return 2 * ((p * r) / (p + r + K.epsilon()))
+    # 0 - legitimate label
+    # 1 - spam label
 
+    df = pd.DataFrame(columns=['x', 'y'])  # Dataframe's header definition
 
-# It returns the sms spam dataset splitted into training set and test set
-def load_sms_spam_data(file_path='sms_spam_dataframe.pkl'):
-    try:
-      df = pd.read_pickle(file_path)  # Reading data from the provided path file
-    except Exception as e:
-      #tfds.list_builders()
-
-      # Loading data
-      ds = tfds.load('huggingface:sms_spam', split='train', shuffle_files=True)
-      assert isinstance(ds, tf.data.Dataset)
-
-      # 0 - ham
-      # 1 - spam
-
-      df = pd.DataFrame(columns=['x', 'y'])  # Dataframe's header definition
-
-      # Converting ds into a dataframe
-      for sample in ds:
+    # Converting ds into a dataframe
+    for sample in ds:
         current_sample = {'x': tf.keras.backend.get_value(sample['sms']).decode('utf-8'), 'y': tf.keras.backend.get_value(sample['label'])}
         df = df.append(current_sample, ignore_index=True)
 
-        #print(list(sample.keys()))
-        #print(sample['label'])
-        #print(sample['sms'])
+    #print(list(sample.keys()))
+    #print(sample['label'])
+    #print(sample['sms'])
 
-    X_train, X_test, y_train, y_test = train_test_split(df['x'], df['y'], test_size=0.3, random_state=1)  # Splitting data into training set and test set
+    df.to_csv("dataset.csv", index=False, header=False)  # Save CSV
 
-    # Tokenization process
-    tokenizer = Tokenizer(num_words=None)
-    tokenizer.fit_on_texts(X_train)
-
-    tokenized_X_train = tokenizer.texts_to_sequences(X_train)
-    tokenized_X_test = tokenizer.texts_to_sequences(X_test)
-
-    vocabulary_size = len(tokenizer.word_index) + 1
-    maxlen = max(len(max(tokenized_X_train, key=len)), len(max(tokenized_X_test, key=len)))
-
-    # Padding process
-    X_train = pad_sequences(tokenized_X_train, padding='post', maxlen=maxlen)
-    X_train = np.array(X_train, dtype=np.float)
-    X_test = pad_sequences(tokenized_X_test, padding='post', maxlen=maxlen)
-    X_test = np.array(X_test, dtype=np.float)
-
-    y_train = np.array(y_train, dtype=np.float)
-    y_test = np.array(y_test, dtype=np.float)
-
-    return X_train, y_train, X_test, y_test
+    return df
+'''
