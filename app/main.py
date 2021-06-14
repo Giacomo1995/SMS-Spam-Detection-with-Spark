40716a3e19a955fb6192c168a971b2038193e5ea
Giacomo <giamel95@outlook.it>
2021-06-14 17:41:46 +0200

    Integrated main with new classifiers

--- app/main.py
+++ app/main.py
@@ -19,7 +19,7 @@ from classifier import *
 def main():
     """
     Starts a SparkSession and perform the classification task to detect spam in SMS messages.
-    It prints Accuracy and F-Measure of the model.
+    It prints Accuracy and F-Measure of the models.
     """
 
     findspark.init()
@@ -36,30 +36,65 @@ def main():
 
     training_set, test_set = load_dataset(spark)
 
-    # Initialize Classifier
-    clf = Classifier()
+    # Initialize Classifiers
+    lr_clf = Classifier(classifier="lr", k=3)
+    mlp_clf = Classifier(classifier="mlp", k=3)
+    nb_clf = Classifier(classifier="nb", k=3)
 
-    # Run cross-validation and choose the best set of parameters
-    clf.fit(training_set)
+    # Run cross-validation and choose the best set of parameters for each model
+    lr_clf.fit(training_set)
+    mlp_clf.fit(training_set)
+    nb_clf.fit(training_set)
 
-    # Make predictions on test set on the best model found
-    result = clf.evaluate(test_set)
+    # Make predictions on test set on the best models found
+    lr_result = lr_clf.evaluate(test_set)
+    mlp_result = mlp_clf.evaluate(test_set)
+    nb_result = nb_clf.evaluate(test_set)
+    
 
-    # Best model parameters
-    params = clf.cvModel.bestModel.stages[2]
-    print("MODEL PARAMETERS:")
+    # Print the results
+    print("LOGISTIC REGRESSION")
+    params = lr_cvmodel.bestModel.stages[2]
     print("Number of Features: ", params.numFeatures)
     print(params.explainParam("regParam"))
     print(params.explainParam("elasticNetParam"))
 
-    # Print Accuracy and F-Measure evaluation metrics
-    predictionAndLabels = result.select("prediction", "label")
+    lr_prediction_and_labels = lr_result.select("prediction", "label")
+    lr_evaluator = MulticlassClassificationEvaluator(metricName="accuracy")
+    print("\nAccuracy: " + str(lr_evaluator.evaluate(lr_prediction_and_labels)))
 
-    evaluator = MulticlassClassificationEvaluator(metricName="accuracy")
-    print("\nAccuracy: " + str(evaluator.evaluate(predictionAndLabels)))
+    lr_evaluator = MulticlassClassificationEvaluator(metricName="fMeasureByLabel", metricLabel=1)
+    print("F-Measure: " + str(lr_evaluator.evaluate(lr_prediction_and_labels)))
 
-    evaluator = MulticlassClassificationEvaluator(metricName="fMeasureByLabel", metricLabel=1)
-    print("F-Measure: " + str(evaluator.evaluate(predictionAndLabels)))
+    print("\n--------------------------------------------------------------------------------------------")
+
+    print("\nMLP")
+    params = mlp_cvmodel.bestModel.stages[2]
+    print("Number of Features: ", params.numFeatures)
+    print(params.explainParam("blockSize"))
+    print(params.explainParam("layers"))
+
+    mlp_prediction_and_labels = mlp_result.select("prediction", "label")
+    mlp_evaluator = MulticlassClassificationEvaluator(metricName="accuracy")
+    print("\nAccuracy: " + str(mlp_evaluator.evaluate(mlp_prediction_and_labels)))
+
+    mlp_evaluator = MulticlassClassificationEvaluator(metricName="fMeasureByLabel", metricLabel=1)
+    print("F-Measure: " + str(mlp_evaluator.evaluate(mlp_prediction_and_labels)))
+
+
+    print("\n--------------------------------------------------------------------------------------------")
+
+    print("\nNAIVE BAYES")
+    params = nb_cvmodel.bestModel.stages[2]
+    print("Number of Features: ", params.numFeatures)
+    print(params.explainParam("smoothing"))
+
+    nb_prediction_and_labels = nb_result.select("prediction", "label")
+    nb_evaluator = MulticlassClassificationEvaluator(metricName="accuracy")
+    print("\nAccuracy: " + str(nb_evaluator.evaluate(nb_prediction_and_labels)))
+
+    nb_evaluator = MulticlassClassificationEvaluator(metricName="fMeasureByLabel", metricLabel=1)
+    print("F-Measure: " + str(nb_evaluator.evaluate(nb_prediction_and_labels)))
 
 
 if __name__ == "__main__":
